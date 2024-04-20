6471769a79cb6c52fa7fe4ca3b9decd73202e8ad
Shenfan Feng <99145754+Zayn407@users.noreply.github.com>
2024-04-20 09:06:30 -0500

    Add files via upload

--- exp_script.js
+++ exp_script.js
@@ -26,11 +26,3 @@ window.__gcse = {
 
 // 加载 Google CSE 脚本
 (function() {
-  var cx = '664e348dc06344073';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
